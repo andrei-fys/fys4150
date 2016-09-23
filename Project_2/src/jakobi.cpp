1665075c6851b10a63fd71820c382654a093ec2a
andrei-fys <andreiku@fys.uio.no>
2016-09-23 14:10:18 +0200

    matrix var B -> A

--- Project_2/src/jakobi.cpp
+++ Project_2/src/jakobi.cpp
@@ -10,20 +10,20 @@ void show_matrix (int, double**);
 
 int main(int argc, char* argv[]){
 	int N = atof(argv[1]);
-	double ** B = new double*[N];
+	double ** A = new double*[N];
 	for (int i=0;i<N;i++){
-		B[i] = new double[N];
+		A[i] = new double[N];
 	}
 	for (int i=0;i<N;i++){
 		for (int j=0;j<N;j++){
-		B[i][j]=1.0;
+		A[i][j]=1.0;
 		}
 	}
-	show_matrix(N, B);
+	show_matrix(N, A);
 	for (int i=0;i<N;i++){
-		delete B[i];
+		delete A[i];
 	}
-	delete[] B;
+	delete[] A;
 }
 
 void show_matrix (int n, double ** matrix){
