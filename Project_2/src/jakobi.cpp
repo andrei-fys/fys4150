9bcce7ce2e8e7604f9fea57bcd44203a631f0b8b
andrei-fys <andreiku@fys.uio.no>
2016-09-23 16:09:44 +0200

    finding max abs. off-diag element function

--- Project_2/src/jakobi.cpp
+++ Project_2/src/jakobi.cpp
@@ -7,6 +7,7 @@
 using namespace std;
 
 void show_matrix (int, double**);
+double max_offdiag(int, double**);
 
 int main(int argc, char* argv[]){
 	int N = atof(argv[1]);
@@ -16,16 +17,21 @@ int main(int argc, char* argv[]){
 	}
 	for (int i=0;i<N;i++){
 		for (int j=0;j<N;j++){
-		A[i][j]=1.0;
+		A[i][j]=rand() % 19 + (-9);
 		}
 	}
+	A[2][2]=10000;
+	A[1][4]=-28.0;
+	double max_element = max_offdiag(N, A);
 	show_matrix(N, A);
+	cout << "Max element is " << max_element << endl;
 	for (int i=0;i<N;i++){
 		delete A[i];
 	}
 	delete[] A;
 }
 
+/* Just prints matrix */
 void show_matrix (int n, double ** matrix){
 	for (int i=0;i<n;i++){
 		for (int j=0;j<n;j++){
@@ -33,4 +39,19 @@ void show_matrix (int n, double ** matrix){
 		}
 	cout << endl;
 	}
+}
+
+/* Returns maximal offdiagonal element */
+double max_offdiag(int n, double ** matrix){
+	double max = 0.0;
+	for (int i=0;i<n;i++){
+		for (int j=0;j<n;j++){
+			if (abs(i-j)>=1){
+				if (abs(matrix[i][j]) >= max ){
+				max = abs(matrix[i][j]);
+				}
+			}
+		}
+	}
+	return max;
 }
