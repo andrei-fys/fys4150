175155704a66848372d79e5c41e114f2ac5aad20
andrei-fys <andreiku@fys.uio.no>
2016-09-26 16:00:07 +0200

    deleted bound cond from matrix. fill in matrix

--- Project_2/src/jakobi.cpp
+++ Project_2/src/jakobi.cpp
@@ -13,36 +13,31 @@ int main(int argc, char* argv[]){
 	// first arg is number of G.P.; second - ro_max
 	int N = atof(argv[1]);
 	double ** A = new double*[N];
+	for (int i=0;i<N;i++){
+		A[i] = new double[N];
+	}
 	double ro_null = 0.0;
 	double ro_max = atof(argv[2]);
 	double fake_zero = 1e-7;
-	double h = (ro_max - ro_null)/(N - 1);
+	double h = (ro_max - ro_null)/(N);
 	double h_1 = 1.0/(h*h);
 	double h_2 = 2.0/(h*h);
 	//Dirichlet bound. cond.
 	double * ro = new double[N];
-	ro[0] = 0.0;
-	ro[N-1] = 0.0;
-	for (int i=1; i<=N-2; i++) {
-		ro[i] = ro[i-1] + i*h;
+	for (int i=0; i<=N-1; i++) {
+		ro[i] = ro_null + i*h;
 	}
 	double * V = new double[N];
-	V[0] = 0.0;
-	V[N-1] = 0.0;
-	for (int i=1; i<=N-2; i++) {
+	for (int i=0; i<=N-1; i++) {
 		V[i] = ro[i]*ro[i];
 	}
-
-	for (int i=0;i<N;i++){
-		A[i] = new double[N];
-	}
-	for (int i=0;i<N;i++){
-		for (int j=0;j<N;j++){
-		A[i][j]=rand() % 19 + (-9);
+	for (int i = 0; i <= N-1; i++) {
+		for (int j = 0; j <= N-1; j++){
+			if (i == j) A[i][j] = h_2 + V[i];
+			if (abs(i - j) == 1) A[i][j] = -h_1;
 		}
 	}
-	A[2][2]=10000;
-	A[1][4]=-28.0;
+	A[3][4]=-2000.0;
 	int max_i, max_j;
 	max_offdiag(N, A, max_i, max_j);
 	show_matrix(N, A);
