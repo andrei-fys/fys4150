9a558ee974acfc3803fa6e340b17f6566381d983
andrei-fys <andreiku@fys.uio.no>
2016-09-26 17:27:51 +0200

    Jacoby-goes-round

--- Project_2/src/jakobi.cpp
+++ Project_2/src/jakobi.cpp
@@ -8,6 +8,7 @@ using namespace std;
 
 void show_matrix (int, double**);
 void max_offdiag(int, double**, int &, int &);
+void Jacobi_goes_round(int, double **, int, int);
 
 int main(int argc, char* argv[]){
 	// first arg is number of G.P.; second - ro_max
@@ -37,9 +38,11 @@ int main(int argc, char* argv[]){
 			if (abs(i - j) == 1) A[i][j] = -h_1;
 		}
 	}
-	A[3][4]=-2000.0;
+	//A[3][4]=-2000.0;
 	int max_i, max_j;
 	max_offdiag(N, A, max_i, max_j);
+	show_matrix(N, A);
+	Jacobi_goes_round(N, A, max_i, max_j);
 	show_matrix(N, A);
 	cout << "Max element is " << max_i << "  " << max_j << endl;
 	for (int i=0;i<N;i++){
@@ -72,4 +75,48 @@ void max_offdiag(int n, double ** matrix, int& max_i, int& max_j){
 			}
 		}
 	}
+}
+
+void Jacobi_goes_round(int n, double** a, int k, int l){
+	double tau = (a[l][l]-a[k][k])/(2.0*a[k][l]);
+	double t;
+	if (tau < 0){
+		t = -tau - sqrt(1 + tau*tau);
+	} else {
+		t = -tau + sqrt(1 + tau*tau);
+	}
+	double c = 1/(sqrt(1 + t*t));
+	double s = c*t;
+	double ** b = new double*[n];
+	for (int i=0;i<n;i++){
+		b[i] = new double[n];
+	}
+	for (int i = 0; i <= n-1; i++) {
+		for (int j = 0; j <= n-1; j++){
+			b[i][j]=a[i][j];
+		}
+	}
+	for (int i = 0; i <= n-1; i++) {
+		for (int j = 0; j <= n-1; j++){
+			b[k][k]=a[k][k]*c*c - 2*a[k][l]*c*s + a[l][l]*s*s;
+			b[l][l]=a[l][l]*c*c + 2*a[k][l]*c*s + a[k][k]*s*s;
+			//b[k][l]=(a[k][k] - a[l][l])*c*s + a[k][l]*(c*c - s*s);
+			b[l][k]=b[k][l]=0.0;
+			if (i != k && i != l ) {
+			b[i][i]=a[i][i];
+			b[i][k]=a[i][k]*c - a[i][l]*s;
+			b[i][l]=a[i][l]*c + a[i][k]*s;
+			}
+		}
+	}	
+	for (int i = 0; i <= n-1; i++) {
+		for (int j = 0; j <= n-1; j++){
+			a[i][j]=b[i][j];
+		}
+	}
+	for (int i=0;i<n;i++){
+		delete b[i];
+	}
+	delete[] b;
+
 }
