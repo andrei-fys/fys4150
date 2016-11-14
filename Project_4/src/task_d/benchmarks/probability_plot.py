d786fd58ba354e6d8c0a0e1e8bc632d81c7c79bc
Anna Gribkovskaya <anna.gribkovskaya@gmail.com>
2016-11-14 20:43:50 +0100

    some plots

---
+++ Project_4/src/task_d/benchmarks/probability_plot.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+filename1 = sys.argv[1]
+#filename2 = sys.argv[2]
+
+data1 = np.loadtxt(filename1,  delimiter=',')
+#data2 = np.loadtxt(filename2,  delimiter=',')
+
+
+
+fig1 = plt.figure()
+ax1 = fig1.add_subplot(111)
+#ax1.plot(data1[:,0],data1[:,1],color="black",label=r'$E$')
+#ax1.plot(data1[:,0],data1[:,2],color="red",label=r'$|M|$')
+a = np.amax(data1[:,1])
+ax1.bar(data1[:,0],data1[:,1]/a, 0.01, color="blue",label=r'$20$')
+#ax1.plot(data2[:,0],data2[:,2],color="red",label=r'$40$')
+#ax1.plot(data1[:,0],data1[:,4],color="green",label=r'$\Xi$')
+#x = data1[:,0]
+#y = data1[:,1]
+
+plt.grid()
+#plt.legend(loc="upper right", fontsize=18)
+
+plt.xlabel(r'Energy per spin', fontsize=24)
+plt.ylabel(r'Probability', fontsize=24)
+#plt.bar (x,y)
+plt.draw()
+#plt.show()
+plt.savefig('prob.pdf')
+Energy = np.array (data1[:,0])
+Prob = np.array(data1[:,1]/a)
+
+
