926f4b165fc23fd1203d2ce96fa8e2a2cfc4bd98
andrei-fys <andreiku@fys.uio.no>
2016-11-25 10:24:13 +0100

    initial working, unit test passed for non-int case

--- Project_5/src/vmc.cpp
+++ Project_5/src/vmc.cpp
@@ -7,7 +7,7 @@
 #include <string>
 using namespace std;
 
-void transition_probability(double, double *, double *, double *, double *, double &);
+void transition_probability(double, double, double *, double *, double *, double *, double &);
 void update_local_energy(double, double *, double *, double, double &); 
 void Metropolis(int, double, double, double *, double *,double *, double *, double &, double &, double, double &);
 
@@ -39,9 +39,9 @@ int main(int argc, char* argv[]){
 	double W = 0.0;
 	// physics comes here
 	double omega = 1.0;   // from keyboard
-	double alpha = 0.05;   // AAAAAA!!!!!!
-	double h = 5.0;       // step
-	double E_L = 0.0;
+	double alpha = 1.0;   // AAAAAA!!!!!!
+	double h = 1.0;       // step
+	double local_energy = 0.0;
 	double expectation_energy = 0.0;
 	R1[0] = 1.0;
 	R1[1] = 1.0;
@@ -51,39 +51,44 @@ int main(int argc, char* argv[]){
 	R2[2] = 1.0;
 
 	//while (T <= T_finish){ here should be alpha-loop
-	Metropolis(MC_samples, omega, alpha, R1, R2, R1_new, R2_new, W, E_L, h, expectation_energy);
+	Metropolis(MC_samples, omega, alpha, R1, R2, R1_new, R2_new, W, local_energy, h, expectation_energy);
 	//new appha
 	//}
 	
 	delete[] R1;
 	delete[] R2;
+	delete[] R1_new;
+	delete[] R2_new;
 }
 
-void transition_probability(double omega, double * R1, double * R2,
+
+void transition_probability(double omega, double alpha, double * R1, double * R2,
 							double * R1_new, double * R2_new, double &W){
-	W = exp(omega*(R2_new[0]*R2_new[0] + R2_new[1]*R2_new[1] + R2_new[2]*R2_new[2] +
-				R1_new[0]*R1_new[0] + R1_new[1]*R1_new[1] + R1_new[2]*R1_new[2] - 
-				R1[0]*R1[0] - R1[1]*R1[1] - R1[2]*R1[2] -
-				R2[0]*R2[0] - R2[1]*R2[1] - R2[2]*R2[2]));
+	W = exp(alpha*omega*(R1[0]*R1[0] + R1[1]*R1[1] + R1[2]*R1[2] +
+						 R2[0]*R2[0] + R2[1]*R2[1] + R2[2]*R2[2] -
+						R2_new[0]*R2_new[0] - R2_new[1]*R2_new[1] - R2_new[2]*R2_new[2] -
+						R1_new[0]*R1_new[0] - R1_new[1]*R1_new[1] - R1_new[2]*R1_new[2]
+						));
 
 }
 
 void update_local_energy(double omega, double * R1, double * R2, 
-					double alpha, double &E_L){
+					double alpha, double & local_energy){
 	double R1_sqrt = R1[0]*R1[0] + R1[1]*R1[1] + R1[2]*R1[2];
 	double R2_sqrt = R2[0]*R2[0] + R2[1]*R2[1] + R2[2]*R2[2];
-	E_L = 0.5*omega*omega*(R1_sqrt + R2_sqrt)*(1.0 - alpha*alpha) + 3.0*alpha*omega; 
+	local_energy = 0.5*omega*omega*(R1_sqrt + R2_sqrt)*(1.0 - alpha*alpha) + 3.0*alpha*omega; 
 
 }
 
 void Metropolis(int MC_samples, double omega, double alpha, double * R1, double * R2,
-				double * R1_new, double * R2_new, double &W, double &E_L, double h, double & expectation_energy){
+				double * R1_new, double * R2_new, double &W, double &local_energy, double h, double & expectation_energy){
 	// Initialize the seed and call the Mersienne algo
 	random_device rd;
 	mt19937_64 gen(rd());
 	uniform_real_distribution<double> RandomNumberGenerator(-1.0,1.0);
 	uniform_real_distribution<double> RandomNumberGenerator1(0.0,1.0);
 	// MC ragarded vars
+	double mean_energy = 0;
 	int MC_counter = 0;
 	int MC_rejected = 0;
 	int MC_accepted = 0;
@@ -94,7 +99,7 @@ void Metropolis(int MC_samples, double omega, double alpha, double * R1, double 
 		R1_new[0] = R1[0] + (RandomNumberGenerator(gen)*h);
 		R1_new[1] = R1[1] + (RandomNumberGenerator(gen)*h);
 		R1_new[2] = R1[2] + (RandomNumberGenerator(gen)*h);
-		transition_probability(omega, R1, R2, R1_new, R2_new, W);
+		transition_probability(omega, alpha, R1, R2, R1_new, R2_new, W);
 			if (W >= 1.0){
 				R1[0] = R1_new[0];
 				R1[1] = R1_new[1];
@@ -102,12 +107,16 @@ void Metropolis(int MC_samples, double omega, double alpha, double * R1, double 
 				R2[0] = R2_new[0];
 				R2[1] = R2_new[1];
 				R2[2] = R2_new[2];
-				update_local_energy(omega, R1, R2, alpha, E_L);
+				//mean_energy += update_local_energy(omega, R1, R2, alpha);
+				update_local_energy(omega, R1, R2, alpha, local_energy);
+				mean_energy += local_energy;
 				MC_accepted++;
 			} else {
 				double sampling_parameter = RandomNumberGenerator1(gen);
 				if (W < sampling_parameter){
-					//update_local_energy(omega, R1, R2, alpha, E_L);
+					update_local_energy(omega, R1, R2, alpha, local_energy);
+					//mean_energy += update_local_energy(omega, R1, R2, alpha);
+					mean_energy += local_energy;
 					MC_rejected++;
 				} else {
 					R1[0] = R1_new[0];
@@ -116,16 +125,20 @@ void Metropolis(int MC_samples, double omega, double alpha, double * R1, double 
 					R2[0] = R2_new[0];
 					R2[1] = R2_new[1];
 					R2[2] = R2_new[2];
-					update_local_energy(omega, R1, R2, alpha, E_L);
+					//mean_energy += update_local_energy(omega, R1, R2, alpha);
+					update_local_energy(omega, R1, R2, alpha, local_energy);
+					mean_energy += local_energy;
 					MC_accepted++;
 				}
 			}
+	cout << R1[0] << endl;
+	cout << R1[1] << endl;
+	cout << R1[2] << endl;
 	MC_counter++;
 	}
-	expectation_energy = E_L/MC_samples;
 	cout << "Accept " <<MC_accepted << endl;
 	cout << "Reject " <<MC_rejected << endl;
-	cout << "Local energy " << E_L << endl;
+	cout << "Local energy " << mean_energy/MC_samples << endl;
 }
 
 
