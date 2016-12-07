c8702251e01d827ec98745051a31b6a16ab7d2a3
Anna Gribkovskaya <anna.gribkovskaya@gmail.com>
2016-12-07 20:55:54 +0100

    relative distance fixed

--- Project_5/src/vmc2.cpp
+++ Project_5/src/vmc2.cpp
@@ -53,9 +53,9 @@ int main(int argc, char* argv[]){
 	R2[0] = -1.0;
 	R2[1] = -1.0;
 	R2[2] = 1.0;
-	double R12 = sqrt(abs(R1[0]*R1[0] + R1[1]*R1[1] + R1[2]*R1[2] - R2[0]*R2[0] + R2[1]*R2[1] + R2[2]*R2[2]));
+	double R12 = sqrt(pow(R1[0]-R2[0],2)+pow(R1[1]-R2[1],2)+pow(R1[2]-R2[2],2));
 	double R12_new;		
-	double mean_distance = 0;
+	double mean_distance = R12;
 	//find optimal step
 	const int n = 500; // Number of iterations to find step
 	double step = 0.01;
@@ -64,7 +64,7 @@ int main(int argc, char* argv[]){
 	double mean_h = 0;
 	for (int j=0; j < 10; j++) {
 	for ( int i=0; i < n; i++ ){
-		Metropolis(MC_samples, omega, alpha, R1, R2, R1_new, R2_new, R12_new,  W,  local_energy, h,  expectation_energy, 				expectation_energy_squared, MC_rejected_prosent, MC_accepted_prosent, variance,  R12, mean_distance, beta);
+		Metropolis(MC_samples_step, omega, alpha, R1, R2, R1_new, R2_new, R12_new,  W,  local_energy, h,  expectation_energy, 				expectation_energy_squared, MC_rejected_prosent, MC_accepted_prosent, variance,  R12, mean_distance, beta);
 		//cout << "rejected  " << MC_rejected_prosent << endl;
 		if (abs(MC_rejected_prosent - 50.0) < 1.0 ){
 			//cout << "optimal h  " << h << endl;
@@ -94,14 +94,14 @@ int main(int argc, char* argv[]){
 	R1[0] = 1.0;
 	R1[1] = 1.0;
 	R1[2] = 1.0;
-	R2[0] = -1.0;
+	R2[0] = -1.1;
 	R2[1] = -1.0;
-	R2[2] = 1.0;
+	R2[2] = 1.1;
 	//##############################
 	Metropolis(MC_samples, omega, alpha, R1, R2, R1_new, R2_new, R12_new,  W,  local_energy, h,  expectation_energy, 				expectation_energy_squared, MC_rejected_prosent, MC_accepted_prosent, variance,  R12, mean_distance, beta);
 	cout << "Final rejected  " << MC_rejected_prosent << endl;
 	cout << "Expectation energy: " << expectation_energy << endl;
-	cout << "Relative distance expectation " << R12/MC_samples << endl;
+	cout << "Relative distance expectation " << mean_distance/MC_samples << endl;
 	file_writer(output_filename,  expectation_energy, variance, alpha);
 //new appha
 	//}
@@ -127,22 +127,22 @@ void update_local_energy(double omega, double * R1, double * R2, double alpha, d
 
 	double R1_sq = R1[0]*R1[0] + R1[1]*R1[1] + R1[2]*R1[2];
 	double R2_sq = R2[0]*R2[0] + R2[1]*R2[1] + R2[2]*R2[2];
-	double R12 = sqrt(abs(R1_sq - R2_sq));
+	double R12 = sqrt(pow(R1[0]-R2[0],2)+pow(R1[1]-R2[1],2)+pow(R1[2]-R2[2],2));
 	local_energy = 0.5*omega*omega*(R1_sq + R2_sq)*(1.0 - alpha*alpha) + 3.0*alpha*omega + 1/R12 +
 			+ 1/(2*(1+beta*R12)*(1+beta*R12))*(alpha*omega*R12 - 1/(2*(1+beta*R12)*(1+beta*R12)) - 2/R12 + 2*beta/(1+beta*R12));
 }
 
 
 void compute_distance(double * R1, double * R2, double & R12){
-	double R1_sq = R1[0]*R1[0] + R1[1]*R1[1] + R1[2]*R1[2];
-	double R2_sq = R2[0]*R2[0] + R2[1]*R2[1] + R2[2]*R2[2];
-	R12 = sqrt(abs(R1_sq - R2_sq));
+//	double R1_sq = R1[0]*R1[0] + R1[1]*R1[1] + R1[2]*R1[2];
+//	double R2_sq = R2[0]*R2[0] + R2[1]*R2[1] + R2[2]*R2[2];
+	R12 = sqrt(pow(R1[0]-R2[0],2)+pow(R1[1]-R2[1],2)+pow(R1[2]-R2[2],2));
 }
 
 void compute_new_distance(double * R1_new, double * R2_new, double & R12_new){
-	double R1new_sq = R1_new[0]*R1_new[0] + R1_new[1]*R1_new[1] + R1_new[2]*R1_new[2];
-	double R2new_sq = R2_new[0]*R2_new[0] + R2_new[1]*R2_new[1] + R2_new[2]*R2_new[2];
-	R12_new = sqrt(abs(R1new_sq - R2new_sq));
+//	double R1new_sq = R1_new[0]*R1_new[0] + R1_new[1]*R1_new[1] + R1_new[2]*R1_new[2];
+//	double R2new_sq = R2_new[0]*R2_new[0] + R2_new[1]*R2_new[1] + R2_new[2]*R2_new[2];
+	R12_new = sqrt(pow(R1_new[0]-R2_new[0],2)+pow(R1_new[1]-R2_new[1],2)+pow(R1_new[2]-R2_new[2],2));
 }
 
 
@@ -233,4 +233,5 @@ void file_writer(char* filename, double expectation_energy, double variance, dou
 	ofile << alpha << "," << expectation_energy << "," << variance << endl;
 	ofile.close();
 }
+
 
